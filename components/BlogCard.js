bee547f68eccc46dcd252185598e206aa11b18a2
Mehranlip <realmehranasadi@gamil.com>
2022-11-26 05:38:20 -0800

    end style card

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,17 +1,32 @@
-import Link from "next/link"
-import styles from "../styles/BlogCard.module.css"
+import Link from "next/link";
+import styles from "../styles/BlogCard.module.css";
 
-
-export default function BlogPost({ title, author, coverPhoto, datePublished, slug }) {
-    console.log(coverPhoto);
-    return (
-        <div className={styles.card}>
-            <Link href={"/posts/" + slug}>
-                <div className={styles.imgContainer}>
-                    <img src={coverPhoto.url} alt="" />
-                </div>
-            </Link>
-
+export default function BlogPost({
+  title,
+  author,
+  coverPhoto,
+  datePublished,
+  slug,
+}) {
+  return (
+    <div className={styles.card}>
+      <Link href={"/posts/" + slug}>
+        <div className={styles.imgContainer}>
+          <img src={coverPhoto.url} alt="" />
         </div>
-    )
+      </Link>
+      <div className={styles.text}>
+        <h2>{title}</h2>
+        <div className={styles.details}>
+          <div className={styles.author}>
+            <img src={author.avatar.url} />
+            <h3>{author.name}</h3>
+          </div>
+          <div className={styles.date}>
+            <h3>{datePublished}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
